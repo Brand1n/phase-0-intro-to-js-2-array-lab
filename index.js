11ea4615e1d5ca840797bc7877c8bbf7c7f24fc3
Brandon Tucker <Brandon064@outlook.com>
2023-10-27 06:03:38 -0700

    array

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+
+   
+    
+const destructivelyAppendCat=function(name){
+cats.push("Ralph")
+
+}
+
+const destructivelyPrependCat=function(name){
+    cats.unshift("Bob")
+    }
+    const destructivelyRemoveFirstCat=function(name){
+        cats.shift()
+        }
+const destructivelyRemoveLastCat=function(nmae){
+    cats.pop()
+}
+
+
+
+
+        const appendCat=function(name){
+    
+            const copyofcats = cats.slice()
+            copyofcats.push("Broom")
+            return(copyofcats)
+           }
+           const prependCat=function(name){
+            const copyofcats = cats.slice()
+            copyofcats.unshift("Arnold")
+            return(copyofcats)
+           }
+        const removeLastCat=function(name){
+            const copyofcats = cats.slice()
+            copyofcats.pop("Garfield")
+            return(copyofcats)
+        }
+        const removeFirstCat=function(name){
+            const copyofcats = cats.slice()
+            copyofcats.shift("Milo")
+            return(copyofcats)
+        }
